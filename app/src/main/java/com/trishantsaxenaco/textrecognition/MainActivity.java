9aef9a479ea3d0ec9dac28a27acb44d04daa6a1c
trishantsaxenaco <trishantsaxena@gmail.com>
2020-11-23 16:27:52 +0530

    Added all the library, dependency and classpathin gradle and manifest files, It build succesfully. Added all files

--- app/src/main/java/com/trishantsaxenaco/textrecognition/MainActivity.java
+++ app/src/main/java/com/trishantsaxenaco/textrecognition/MainActivity.java
@@ -2,13 +2,58 @@ package com.trishantsaxenaco.textrecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static Button capture, detect;
+    private static TextView display;
+    private static ImageView image;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        capture = (Button)findViewById(R.id.capture);
+        detect =(Button)findViewById(R.id.detectText);
+        display = (TextView)findViewById(R.id.showText);
+        image = (ImageView)findViewById(R.id.imageview);
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image.setImageBitmap(imageBitmap);
+        }
     }
 }
