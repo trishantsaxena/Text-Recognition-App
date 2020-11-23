6286840089671ae4948946282fbb5a52b0387321
trishantsaxenaco <trishantsaxena@gmail.com>
2020-11-23 17:46:45 +0530

    This is complete running application will be make changes in future

--- app/src/main/java/com/trishantsaxenaco/textrecognition/MainActivity.java
+++ app/src/main/java/com/trishantsaxenaco/textrecognition/MainActivity.java
@@ -1,5 +1,6 @@
 package com.trishantsaxenaco.textrecognition;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
@@ -8,16 +9,29 @@ import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static Button capture, detect;
     private static TextView display;
     private static ImageView image;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +45,20 @@ public class MainActivity extends AppCompatActivity {
         capture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                dispatchTakePictureIntent();
 
             }
         });
+        detect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detectTextfromImage();
+                display.setText("");
+            }
+        });
     }
+
+
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -52,8 +74,41 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+             imageBitmap = (Bitmap) extras.get("data");
             image.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void detectTextfromImage() {
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
+        firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                displayTextfromImage(firebaseVisionText);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,"Error: "+e.getMessage(),Toast.LENGTH_SHORT).show();
+                Log.d("Error: ",e.getMessage());
+
+            }
+        });
+    }
+
+    private void displayTextfromImage(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.Block> blockslist = firebaseVisionText.getBlocks();
+        if (blockslist.size()==0) {
+            Toast.makeText(MainActivity.this,"Text not found!",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            for (FirebaseVisionText.Block block: firebaseVisionText.getBlocks()){
+                String text =block.getText();
+                display.setText(text); //set the charsequence method here
+                                       //if there's any problem then replace with resId method.
+            }
         }
     }
 }
